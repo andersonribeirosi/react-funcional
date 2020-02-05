3b92c0aa1580b2adb63c5d20c0d3d8ce7d248447
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-04 23:54:23 -0300

    Trabalhando com state através do useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [num1, setNum1] = useState()
+  const [num2, setNum2] = useState()
+  const [result, setResult] = useState()
+
+  const somar = () => {
+    const n1 = parseInt(num1)
+    const n2 = parseInt(num2)
+
+    setResult( n1 + n2)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={num1} onChange={e => setNum1(e.target.value)} /> <br />
+      <input type="text" value={num2} onChange={e => setNum2(e.target.value)}/> <br />
+      <button className="btn btn-primary" onClick={somar}> Somar </button> <br />
+      <input type="text" value={result} />
     </div>
   );
 }
