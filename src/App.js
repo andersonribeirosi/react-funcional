ca5eba39779f9505e327995364f6c424e92cacd9
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-05 01:57:28 -0300

    Utilizando componente funciona useEffect(Semelhante ComponetDidMount)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
 
-  const [num1, setNum1] = useState()
-  const [num2, setNum2] = useState()
-  const [result, setResult] = useState()
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [result, setResult] = useState('')
 
   const somar = () => {
     const n1 = parseInt(num1)
     const n2 = parseInt(num2)
 
-    setResult( n1 + n2)
+    setResult(n1 + n2)
   }
+
+useEffect(() => {
+  console.log('variavel numero:', num1, num2)
+}, [num1, num2] )
 
   return (
     <div>
       <input type="text" value={num1} onChange={e => setNum1(e.target.value)} /> <br />
-      <input type="text" value={num2} onChange={e => setNum2(e.target.value)}/> <br />
+      <input type="text" value={num2} onChange={e => setNum2(e.target.value)} /> <br />
       <button className="btn btn-primary" onClick={somar}> Somar </button> <br />
       <input type="text" value={result} />
     </div>
---
+++ src/useState.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react'
+
+function UseState() {
+
+    const [num1, setNum1] = useState()
+    const [num2, setNum2] = useState()
+    const [result, setResult] = useState()
+
+    const somar = () => {
+        const n1 = parseInt(num1)
+        const n2 = parseInt(num2)
+
+        setResult(n1 + n2)
+    }
+
+    return (
+        <div>
+            <input type="text" value={num1} onChange={e => setNum1(e.target.value)} /> <br />
+            <input type="text" value={num2} onChange={e => setNum2(e.target.value)} /> <br />
+            <button className="btn btn-primary" onClick={somar}> Somar </button> <br />
+            <input type="text" value={result} />
+        </div>
+    );
+}
+
+export default App;
